359fc21caa5137b1220d8d626101e85e9c8f622b
Frank Becker <fb@alien8.de>
2018-10-27 16:56:00 +0200

    black -S --py36 convert.py
    Signed-off-by: Frank Becker <fb@alien8.de>

--- convert.py
+++ convert.py
@@ -15,6 +15,7 @@ from bs4 import BeautifulSoup
 import teacher_list
 import utils
 
+
 class StandInData(NamedTuple):
     lesson_number: str
     klasse: str
@@ -35,7 +36,6 @@ def parse_row(row=None, fh=None):
     would_have_hour = data[2]
     instead_of = data[3]
     insted_of_statt = data[4]
-    
 
     items_dict = {
         lesson_number: lesson_number.string,
@@ -48,38 +48,42 @@ def parse_row(row=None, fh=None):
     if lesson_number.string == '\xa0':
         pass
     else:
-        data = (items_dict.get(lesson_number))
+        data = items_dict.get(lesson_number)
         fh.write("\n")
         fh.write(data)
         fh.write(":\n")
     if klasse.string == '\xa0':
         pass
     else:
-        data = (items_dict.get(klasse))
+        data = items_dict.get(klasse)
         fh.write(data)
         fh.write(' | ')
     if would_have_hour.string == '\xa0':
         pass
     else:
-        data = (items_dict.get(would_have_hour))
+        data = items_dict.get(would_have_hour)
         fh.write(data)
         fh.write(' | ')
     if instead_of.string == '\xa0':
         pass
     else:
-        data = (items_dict.get(instead_of))
+        data = items_dict.get(instead_of)
         fh.write(data)
         fh.write(" ")
     if instead_of.string == '\xa0':
         pass
     else:
-        data = (items_dict.get(insted_of_statt))
+        data = items_dict.get(insted_of_statt)
         fh.write(data)
         fh.write("\n")
 
-
-    return StandInData(lesson_number.string, klasse.string,
-           would_have_hour.string, instead_of.string, insted_of_statt.string)
+    return StandInData(
+        lesson_number.string,
+        klasse.string,
+        would_have_hour.string,
+        instead_of.string,
+        insted_of_statt.string,
+    )
 
 
 def parse_footer_row(row=None, fh=None):
@@ -101,10 +105,9 @@ def main(file=None):
         raw_file_path = latest_file
 
     cache_file_path = utils.get_cache_file_path()
-    
-    pathlib.Path(os.path.dirname(cache_file_path)).mkdir(parents=True,
-     exist_ok=True)
-    
+
+    pathlib.Path(os.path.dirname(cache_file_path)).mkdir(parents=True, exist_ok=True)
+
     fh = open(cache_file_path, 'w')
 
     with open(raw_file_path) as fp:
@@ -112,12 +115,13 @@ def main(file=None):
         soup = BeautifulSoup(fp, features="html.parser")
 
     tables = soup.findChildren('table', attrs={'class': ['MsoNormalTable', 'tr']})
-    
-    if not tables:
-        print("Could not find the MsoNormalTable table in the HTML document.\n",
-              file=sys.stderr)
-        sys.exit(1)
 
+    if not tables:
+        print(
+            "Could not find the MsoNormalTable table in the HTML document.\n",
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
     my_table = tables[0]
     rows = my_table.findChildren(['tr'])
@@ -125,7 +129,6 @@ def main(file=None):
         print("Could not find any row in the table {my_table}", file=sys.stderr)
         sys.exit(1)
 
-    
     # Parse header
     row = rows.pop(0)
     paragraphs = row.findAll('p')
@@ -140,27 +143,37 @@ def main(file=None):
         'freitag': 'Fr',
     }
 
-    for day in days_list: 
+    for day in days_list:
         if day in header_date.lower():
             start_char = header_date.lower().find(day)
-            header_date = "".join([header_date[:start_char],
-                                   days_list.get(day),
-                                   header_date[start_char+len(day):]])
+            header_date = "".join(
+                [
+                    header_date[:start_char],
+                    days_list.get(day),
+                    header_date[start_char + len(day) :],
+                ]
+            )
         else:
             pass
-            
+
     header_classes = paragraphs[1].find('span')
     header_classes = header_classes.string
-    
+
     header_teacher = paragraphs[2].find('span')
-    header_teacher = (" ".join(str(i).replace('\n', '').replace('<i> </i>', '').replace('\xa0', '') for i in header_teacher.contents))
+    header_teacher = " ".join(
+        str(i).replace('\n', '').replace('<i> </i>', '').replace('\xa0', '')
+        for i in header_teacher.contents
+    )
     for teacher in teacher_list.teacher_list:
         if teacher in header_teacher.lower():
             start_char = header_teacher.lower().find(teacher)
-            header_teacher = "".join([header_teacher[:start_char],
-                                      
-                                      teacher_list.teacher_list.get(teacher),
-                                      header_teacher[start_char+len(teacher):]])
+            header_teacher = "".join(
+                [
+                    header_teacher[:start_char],
+                    teacher_list.teacher_list.get(teacher),
+                    header_teacher[start_char + len(teacher) :],
+                ]
+            )
     else:
         pass
 
@@ -185,18 +198,17 @@ def main(file=None):
 
     row_count = 100.5 / row_count
 
-
     for row in rows:
-        
-        i += row_count 
+
+        i += row_count
         if not os.environ.get('UNATTENDED', False):
             sys.stdout.write("\r{}%".format(int(i)))
             sys.stdout.flush()
 
         cells = row.findChildren(['td', 'p'])
-        
+
         if len(row) == 11:
-        
+
             row_stand_in_data = parse_row(row=row, fh=fh)
 
             lesson_number = row_stand_in_data.lesson_number
@@ -207,13 +219,12 @@ def main(file=None):
 
             stand_in_data[lesson_number].append(row_stand_in_data)
         elif len(row) == 3:
-            parse_footer_row(row=row,fh=fh)
-        
+            parse_footer_row(row=row, fh=fh)
+
         time.sleep(0.01)
 
-
     fh.close()
-    
+
 
 if __name__ == '__main__':
     main()
