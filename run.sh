9f40f56132addf27d8bb34bf13840a90c068940b
Lb <lb@alien8.de>
2018-11-11 17:39:01 +0100

    Deleted run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-python3 /home/leon/github/vertretungsplan/pull_plan.py
-python3 /home/leon/github/vertretungsplan/convert.py
-python3 /home/leon/github/vertretungsplan/vp_bot.py
